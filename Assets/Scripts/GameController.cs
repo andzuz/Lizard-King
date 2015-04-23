c775c6473e7a4857f7ef65a0ef1b16e2058db193
Andrzej laptop <andrz.zuzak@gmail.com>
2015-04-23 22:06:32 +0200

    Spawning: part 1

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public int zPos = 24;
+	public int[] lizardLeftPositions = {-1, 1};
+	public int[] lizardRightPositions = {-2, 2};
+	public Transform hazardObject;
+	public Transform rewardObject;
+
+	void Start () {
+		SpawnWaves ();
+	}
+
+	void SpawnWaves() {
+		SpawnHazards ();
+		SpawnRewards ();
+	}
+
+	void SpawnHazards () {
+		int index = Random.Range (0, xPos.Length-1);
+		Vector3 spawnPosition = new Vector3 (xPos [index], 0.0f, zPos);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (hazardObject, spawnPosition, spawnRotation);
+	}
+
+	void SpawnRewards () {
+		int index = Random.Range (0, xPos.Length-1);
+		Vector3 spawnPosition = new Vector3 (xPos [index], 0.0f, zPos);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (hazardObject, spawnPosition, spawnRotation);
+	}
+
+}
