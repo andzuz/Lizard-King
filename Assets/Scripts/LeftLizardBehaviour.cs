300763f549b4686055d8727367529ee60d1c4caf
Andrzej laptop <andrz.zuzak@gmail.com>
2015-04-23 21:33:37 +0200

    Game's idea changed, two lizards introduced and moving

---
+++ Assets/Scripts/LeftLizardBehaviour.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftLizardBehaviour : MonoBehaviour {
+	
+	public float timeTakenDuringLerp;
+	public float distanceToMove;
+	public LizardType lizardType;
+
+	private int direction;
+	private bool _isLerping;
+	private Vector3 _startPosition;
+	private Vector3 _endPosition;
+	private float _timeStartedLerping;
+
+	void StartLerping() {
+		_isLerping = true;
+		_timeStartedLerping = Time.time;
+
+		_startPosition = transform.position;
+		_endPosition = transform.position + Vector3.right*direction*distanceToMove;
+	}
+
+	void Start() {
+		if (lizardType == LizardType.LEFT) {
+			direction = 1;
+		} else {
+			direction = -1;
+		}
+	}
+
+	void Update() {
+		switch (lizardType) {
+		case LizardType.LEFT:
+			if(Input.GetKeyDown(KeyCode.A)) {
+				ToggleDirection();
+			}
+			break;
+
+		case LizardType.RIGHT:
+			if(Input.GetKeyDown(KeyCode.D)) {
+				ToggleDirection();
+			}
+			break;
+		}
+	}
+
+	private void ToggleDirection() {
+		direction = -direction;
+		StartLerping();
+	}
+
+	//We do the actual interpolation in FixedUpdate(), since we're dealing with a rigidbody
+	void FixedUpdate() {
+		if(_isLerping)
+		{
+			float timeSinceStarted = Time.time - _timeStartedLerping;
+			float percentageComplete = timeSinceStarted / timeTakenDuringLerp;
+		
+			transform.position = Vector3.Lerp (_startPosition, _endPosition, percentageComplete);
+
+			if(percentageComplete >= 1.0f) {
+				_isLerping = false;
+			}
+		}
+	}
+
+	public enum LizardType {
+		LEFT,
+		RIGHT
+	}
+
+}
---
+++ Assets/Scripts/TreeBehaviour.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeBehaviour : MonoBehaviour {
+
+	public float rotationFactor;
+	//private bool isRotating = false;
+	//private float angleSoFar = 0;
+	//private Quaternion targetRotation = Quaternion.identity;
+
+	public float MaxTiltAngle = 20.0f;
+	public float tiltSpeed = 30.0f; // tilting speed in degrees/second
+
+	Vector3 curRot;
+	float maxX;
+	float maxZ;
+	float minX;
+	float minZ;
+
+
+	// Use this for initialization
+	void Start () {
+		kaka();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//if (Input.GetKeyDown(KeyCode.A)) {
+			//RotateTree ();
+			demona();
+			//transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationFactor * Time.deltaTime);
+		//}
+	}
+
+	public void kaka() {
+		// Get initial rotation
+		curRot = this.transform.eulerAngles;
+		// calculate limit angles:
+		maxX = curRot.x + MaxTiltAngle;
+		maxZ = curRot.z + MaxTiltAngle;
+		minX = curRot.x - MaxTiltAngle;
+		minZ = curRot.z - MaxTiltAngle;
+	}
+
+	public void demona() {
+		// "rotate" the angles mathematically:
+		curRot.x += Input.GetAxis("Vertical") * Time.deltaTime * tiltSpeed;
+		curRot.z += Input.GetAxis("Horizontal") * Time.deltaTime * tiltSpeed;                
+		// Restrict rotation along x and z axes to the limit angles:
+		curRot.x = Mathf.Clamp(curRot.x, minX, maxX);
+		curRot.z = Mathf.Clamp(curRot.z, minZ, maxZ);
+		
+		// Set the object rotation
+		this.transform.eulerAngles = curRot;
+	}
+
+	private void RotateTreeByGivenAngle() {
+		Vector3 oldPoint = new Vector3(0.0f,0.0f,0.0f);
+		Vector3 newPoint = new Vector3 (20.0f, 0.0f, 0.0f);
+
+		Vector3 x = Vector3.Cross (oldPoint.normalized, newPoint.normalized);
+		float theta = Mathf.Asin (x.magnitude);
+		Vector3 w = x.normalized * theta / Time.fixedDeltaTime;
+
+		Quaternion q = transform.rotation * rigidbody.inertiaTensorRotation;
+		Vector3 T = q * Vector3.Scale (rigidbody.inertiaTensor, (Quaternion.Inverse (q) * w));
+
+		rigidbody.AddTorque (T, ForceMode.Impulse);
+
+	}
+	
+	private void RotateTree() {
+		/*float horiz = Input.GetAxis ("Horizontal");
+		int direction;
+
+		if (horiz > 0) {
+			direction = 1;
+		} else if (horiz < 0) {
+			direction = -1;
+		} else {
+			direction = 0;
+		}
+
+		transform.Rotate (new Vector3(0.0f, 1.0f, 0.0f), 20.0f * Time.deltaTime);
+		angleSoFar += 20.0f * Time.deltaTime;
+
+		if (angleSoFar >= rotationFactor) {
+			isRotating = true;
+		}*/
+
+		//Transform afterRotation = transform;
+		//afterRotation.Rotate (new Vector3(0.0f, 1.0f, 0.0f), direction * rotationFactor);
+		//transform.rotation = Quaternion.Slerp(transform.rotation, afterRotation.rotation, Time.deltaTime * rotationFactor);
+		//Transform tmp = transform;
+		//tmp.rotation = new Quaternion(transform.x, transform.y, transfo 
+	}
+
+}
