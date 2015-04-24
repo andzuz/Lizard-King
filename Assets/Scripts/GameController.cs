2c77b3a2797b36e67d35958e8dca28d4935c8a6d
Andrzej laptop <andrz.zuzak@gmail.com>
2015-04-24 16:17:25 +0200

    Moved spawning to Spawner

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,34 +6,31 @@ public class GameController : MonoBehaviour {
 	private int speed = 10;
 	private int score = 0;
 	private bool isPaused = false;
-	private BonusController bonusController;
+
+	public BonusController bonusController;
+	public Spawner spawner;
 	public float waveWait;
 	public float spawnWait;
 	public float startWait;
 	public int hazardCount = 5;
-	public int zPos = 24;
-	public int[] xPos = {-2, -1, 1, 2};
-	public Transform hazardObject;
-	public Transform rewardObject;
-	public const int WAVE_HAZARD = 0;
-	public const int WAVE_REWARD = 1;
-	public const int POINTS_TO_BONUS = 20;
-	public const int BOOST_SPEED_AMOUNT = 1;
 	public int maxSpeed;
 
+	public const int POINTS_TO_BONUS = 20;
+	public const int BOOST_SPEED_AMOUNT = 1;
+
+
 	void Start () {
-		InitBonusController ();
-		Debug.Log (bonusController);
+		//InitBonusController ();
 		BeginSpawning ();
 	}
 
-	void InitBonusController() {
+	/*void InitBonusController() {
 		GameObject bonusControllerObject = GameObject.FindWithTag ("Bonus");
 		
 		if (bonusControllerObject != null) {
 			bonusController = bonusControllerObject.GetComponent <BonusController>();
 		}
-	}
+	}*/
 
 	void Update() {
 		if (Input.GetKeyDown (KeyCode.P)) {
@@ -66,39 +63,15 @@ public class GameController : MonoBehaviour {
 			for (int i = 0; i < hazardCount; i++)
 			{
 				if(bonusController.IsSwarmEnabled()) {
-					SpawnReward();
+					spawner.SpawnReward();
 				} else {
-					SpawnRandom();
+					spawner.SpawnRandom();
 				}
 
 				yield return new WaitForSeconds (spawnWait);
 			}
 			yield return new WaitForSeconds (waveWait);
 		}
-	}
-
-	void SpawnRandom() {
-		int choice = Random.Range (0, 2);
-
-		if (choice == WAVE_HAZARD) {
-			SpawnHazard();
-		} else if (choice == WAVE_REWARD) {
-			SpawnReward();
-		}
-	}
-
-	void SpawnHazard () {
-		int index = Random.Range (0, xPos.Length);
-		Vector3 spawnPosition = new Vector3 (xPos [index], 0.0f, zPos);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (hazardObject, spawnPosition, spawnRotation);
-	}
-
-	void SpawnReward () {
-		int index = Random.Range (0, xPos.Length);
-		Vector3 spawnPosition = new Vector3 (xPos [index], 0.0f, zPos);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (rewardObject, spawnPosition, spawnRotation);
 	}
 
 	public int getSpeed() {
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public int zPos = 24;
+	public int[] xPos = {-2, -1, 1, 2};
+	public Transform hazardObject;
+	public Transform rewardObject;
+
+	public const int WAVE_HAZARD = 0;
+	public const int WAVE_REWARD = 1;
+
+	public void SpawnHazard () {
+		int index = Random.Range (0, xPos.Length);
+		Vector3 spawnPosition = new Vector3 (xPos [index], 0.0f, zPos);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (hazardObject, spawnPosition, spawnRotation);
+	}
+	
+	public void SpawnReward () {
+		int index = Random.Range (0, xPos.Length);
+		Vector3 spawnPosition = new Vector3 (xPos [index], 0.0f, zPos);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (rewardObject, spawnPosition, spawnRotation);
+	}
+
+	public void SpawnRandom() {
+		int choice = Random.Range (0, 2);
+		
+		if (choice == WAVE_HAZARD) {
+			SpawnHazard();
+		} else if (choice == WAVE_REWARD) {
+			SpawnReward();
+		}
+	}
+
+}
