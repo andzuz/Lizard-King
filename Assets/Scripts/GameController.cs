f87860b6be6131627f78f39c5e8c7f88be8cfb6b
Andrzej laptop <andrz.zuzak@gmail.com>
2015-04-24 10:19:50 +0200

    Spawning done

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,32 +3,70 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+	public float waveWait;
+	public float spawnWait;
+	public float startWait;
+	public int hazardCount = 5;
 	public int zPos = 24;
 	public int[] xPos = {-2, -1, 1, 2};
 	public Transform hazardObject;
 	public Transform rewardObject;
+	public const int WAVE_HAZARD = 0;
+	public const int WAVE_REWARD = 1;
 
 	void Start () {
-		SpawnWaves ();
+		BeginSpawning ();
 	}
 
-	void SpawnWaves() {
-		SpawnHazards ();
-		SpawnRewards ();
+	void BeginSpawning() {
+		StartCoroutine (SpawnWaves ());
 	}
 
-	void SpawnHazards () {
-		int index = Random.Range (0, xPos.Length-1);
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			for (int i = 0; i < hazardCount; i++)
+			{
+				SpawnRandom();
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
+	}
+
+	void SpawnRandom() {
+		int choice = Random.Range (0, 2);
+
+		if (choice == WAVE_HAZARD) {
+			SpawnHazard();
+		} else if (choice == WAVE_REWARD) {
+			SpawnReward();
+		}
+	}
+
+	void SpawnHazard () {
+		int index = Random.Range (0, xPos.Length);
 		Vector3 spawnPosition = new Vector3 (xPos [index], 0.0f, zPos);
 		Quaternion spawnRotation = Quaternion.identity;
 		Instantiate (hazardObject, spawnPosition, spawnRotation);
 	}
 
-	void SpawnRewards () {
-		int index = Random.Range (0, xPos.Length-1);
+	void SpawnReward () {
+		int index = Random.Range (0, xPos.Length);
 		Vector3 spawnPosition = new Vector3 (xPos [index], 0.0f, zPos);
 		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (hazardObject, spawnPosition, spawnRotation);
+		Instantiate (rewardObject, spawnPosition, spawnRotation);
+	}
+
+	void OnTriggerExit(Collider other) {
+		string tag = other.tag;
+		Debug.Log (tag);
+
+		if (tag.Equals("Hazard") || tag.Equals("Reward")) {
+			Destroy (other.gameObject);
+		}
 	}
 
 }
